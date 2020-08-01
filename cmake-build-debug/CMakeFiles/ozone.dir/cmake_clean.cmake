46b22454a4f7e9a7e14c646c431ab2ba3038e67e
Stefan Roche <stefanroche@hotmail.com>
2020-08-01 09:02:46 +0100

    Add C++ and CMake

---
+++ cmake-build-debug/CMakeFiles/ozone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ozone.dir/main.cpp.o"
+  "ozone"
+  "ozone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ozone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Ozone!" << endl;
+    return 0;
+}
